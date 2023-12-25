2096dfd2b4dc89f80e769ecbe2d20f77a97e8743
Lakshmi Narasimhan <--global>
2023-12-25 21:06:17 +0530

    update 3

--- src/routes/helloRouter.js
+++ src/routes/helloRouter.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const routes = Router();
 
 routes.get("/", async (req, res) => {
-  res.status(200).send({ message: "Hello World 25th dec 6.45 pm!" });
+  res.status(200).send({ message: "Hello World 25th dec 9.06 pm!" });
 });
 
 export default routes;
